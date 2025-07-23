850e38969c4f06a941b69eca6f661f5b4f77ba5b
HACK-WU <2931580128@qq.com>
2025-07-23 05:37:01 +0000

    feat: 调整conftest文件内容

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,2 @@
-import sys
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parent.parent
-
-
 def pytest_configure():
-    # for path in ROOT.iterdir():
-    #     print(path.name)
-
-    # sys.path.insert(0, str(ROOT))
-    print(sys.path)
-
-    print("hello")
-
-
-pytest_configure()
+    pass
